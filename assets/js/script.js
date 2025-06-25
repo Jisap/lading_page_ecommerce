8a9b131b10210c6c03135afff6957a293ca7c3b4
Jorge Matos <jisapjisap@gmail.com>
2025-06-25 19:12:43 +0200

    Add JavaScript functionality for navbar toggle; update styles to manage body overflow when active

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+'use strict'
+
+// add event on multiple elements
+const addEventOnElem = function (elements, eventType, callback) {
+  // for (let i = 0; i < elements.length; i++) {                         // 1. Inicia un bucle que recorre la colección de elementos
+  //   elements[i].addEventListener(eventType, callback);                // 2. A cada elemento individual, le añade un event listener 
+  // }
+  elements.forEach(elem => elem.addEventListener(eventType, callback));
+}
+
+const $header = document.querySelector('[data-header]');
+const $navbar = document.querySelector('[data-navbar]');
+const $navToggler = document.querySelectorAll('[data-nav-toggler]');
+const $overlay = document.querySelector('[data-overlay]');
+const $dropdownToggler = document.querySelector('[data-dropdown-toggler]');
+const $dropdown = document.querySelector('[data-dropdown]');
+const $cartToggler = document.querySelector('[data-cart-toggler]');
+const $cartModal = document.querySelector('[data-cart-modal]');
+
+const toggleNavbar = function () {           // Añade la clase active cada elemento 
+  $navbar.classList.toggle('active');
+  $overlay.classList.toggle('active');
+  document.body.classList.toggle('active');
+}
+
+// Se añaden eventListeners a los botones de menú y X para el evento click -> callback toggleNavbar
+addEventOnElem($navToggler, 'click', toggleNavbar); 
