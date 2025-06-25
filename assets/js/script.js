cf1df2f2f50ccb5c98cacd40682d11665ad84950
Jorge Matos <jisapjisap@gmail.com>
2025-06-25 19:18:14 +0200

    Add dropdown toggle functionality in script.js; enhance event listener management for navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const addEventOnElem = function (elements, eventType, callback) {
   // for (let i = 0; i < elements.length; i++) {                         // 1. Inicia un bucle que recorre la colección de elementos
   //   elements[i].addEventListener(eventType, callback);                // 2. A cada elemento individual, le añade un event listener 
   // }
-  elements.forEach(elem => elem.addEventListener(eventType, callback));
+  elements.forEach(elem => elem.addEventListener(eventType, callback));  // Más moderno 
 }
 
 const $header = document.querySelector('[data-header]');
@@ -20,6 +20,9 @@ const $dropdown = document.querySelector('[data-dropdown]');
 const $cartToggler = document.querySelector('[data-cart-toggler]');
 const $cartModal = document.querySelector('[data-cart-modal]');
 
+
+//NAVBAR
+
 const toggleNavbar = function () {           // Añade la clase active cada elemento 
   $navbar.classList.toggle('active');
   $overlay.classList.toggle('active');
@@ -28,3 +31,13 @@ const toggleNavbar = function () {           // Añade la clase active cada elem
 
 // Se añaden eventListeners a los botones de menú y X para el evento click -> callback toggleNavbar
 addEventOnElem($navToggler, 'click', toggleNavbar); 
+
+// DROPDOWN
+
+const toggleElem = function (elem) {
+  elem.classList.toggle('active');
+}
+
+$dropdownToggler.addEventListener('click', function () {
+  toggleElem($dropdown);
+});
